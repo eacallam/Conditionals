c86d47b052ac3aa835c612bff27d588009e65fc9
eacallam <eacallam@yahoo.com>
2017-08-17 20:38:43 -0400

    First Commit

---
+++ 08.17.17 Conditionals/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._17._17_Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
